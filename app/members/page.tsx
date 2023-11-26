49b982ba726db08f49a96bdd1b04539cd78cce6f
sec-hkn <151991370+sec-hkn@users.noreply.github.com>
2023-11-26 21:55:57 +0530

    Update page.tsx
    STUDENT TITLE

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -139,7 +139,7 @@ const Testimonials = () => {
         <br/>
         <br/>
         <br/>
-        <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-body-color sm:text-dark md:text-2xl">
+        <p className="dark:text-body-color-dark mb-12 font-bold text-base !leading-relaxed text-body-color sm:text-dark md:text-2xl">
           Student Members
         </p>
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
